b752da0007af369ae5dedda87e07f1435f626e8a
mdparvex <mdparvex443@gmail.com>
2022-09-15 12:27:05 +0600

    simple

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,12 @@
 import requests
 
 endpoint= "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/"
+endpoint = "https://httpbin.org/anything"
 
-get_response = requests.get(endpoint) #HTTP request
+get_response = requests.get(endpoint, json={"query":"hello world"}) #HTTP request
 print(get_response.text) # source code or text raw response
+
+#HTTP request send -> HTML
+#REST API request sends -.=> Json
+
+print(get_response.json())
